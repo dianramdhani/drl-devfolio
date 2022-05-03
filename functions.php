c28e67c8693f3939ffd4ce3950d599edcaaf5c1c
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-05-03 15:03:15 +0000

    init project

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function drl_devfolio_enqueue_styles_scripts()
+{
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap-icons', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/bootstrap-icons.css');
+    wp_enqueue_style('glightbox', get_template_directory_uri() . '/assets/vendor/glightbox/css/glightbox.min.css');
+    wp_enqueue_style('swiper', get_template_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.css');
+    wp_enqueue_style('devfolio', get_template_directory_uri() . '/assets/css/style.css');
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script('purecounter', get_template_directory_uri() . '/assets/vendor/purecounter/purecounter.js', array(), null, true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array(), null, true);
+    wp_enqueue_script('glightbox', get_template_directory_uri() . '/assets/vendor/glightbox/js/glightbox.min.js', array(), null, true);
+    wp_enqueue_script('swiper', get_template_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.js', array(), null, true);
+    wp_enqueue_script('typed', get_template_directory_uri() . '/assets/vendor/typed.js/typed.min.js', array(), null, true);
+    wp_enqueue_script('php-email-form', get_template_directory_uri() . '/assets/vendor/php-email-form/validate.js', array(), null, true);
+    wp_enqueue_script('devfolio', get_template_directory_uri() . '/assets/js/main.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'drl_devfolio_enqueue_styles_scripts');
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>DevFolio Bootstrap Portfolio Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="<?= get_template_directory_uri(); ?>/assets/img/favicon.png" rel="icon">
+    <link href="<?= get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <?php wp_head(); ?>
+
+    <!-- =======================================================
+  * Template Name: DevFolio - v4.7.1
+  * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
