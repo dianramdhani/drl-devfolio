6e9c2869a59be0bd471a83272c9d4c9cf160a71a
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-05-03 15:40:01 +0000

    active all template parts

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <main id="main">
   <?php
   get_template_part('template-parts/about');
-  // get_template_part('template-parts/services');
-  // get_template_part('template-parts/counter');
-  // get_template_part('template-parts/portfolio');
-  // get_template_part('template-parts/testimonials');
-  // get_template_part('template-parts/blog');
-  // get_template_part('template-parts/contact');
+  get_template_part('template-parts/services');
+  get_template_part('template-parts/counter');
+  get_template_part('template-parts/portfolio');
+  get_template_part('template-parts/testimonials');
+  get_template_part('template-parts/blog');
+  get_template_part('template-parts/contact');
   ?>
 </main><!-- End #main -->
 
