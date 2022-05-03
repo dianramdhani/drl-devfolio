cbb658199d582aa9a2edae32fad6ea0af684520c
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-05-03 15:33:50 +0000

    slicing layout to template parts

--- header.php
+++ header.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>DevFolio Bootstrap Portfolio Template - Index</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+  <title><?php bloginfo('name'); ?></title>
 
-    <!-- Favicons -->
-    <link href="<?= get_template_directory_uri(); ?>/assets/img/favicon.png" rel="icon">
-    <link href="<?= get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <?php if (get_bloginfo('description')) : ?>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+  <?php endif; ?>
+  <meta content="" name="keywords">
 
-    <?php wp_head(); ?>
+  <!-- Favicons -->
+  <link href="<?= get_template_directory_uri(); ?>/assets/img/favicon.png" rel="icon">
+  <link href="<?= get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-    <!-- =======================================================
+  <?php wp_head(); ?>
+
+  <!-- =======================================================
   * Template Name: DevFolio - v4.7.1
   * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
   * Author: BootstrapMade.com
@@ -24,3 +27,43 @@
 </head>
 
 <body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="#hero"><?php bloginfo('name'); ?></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="<?= get_template_directory_uri(); ?>/assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#work">Work</a></li>
+          <li><a class="nav-link scrollto " href="#blog">Blog</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
