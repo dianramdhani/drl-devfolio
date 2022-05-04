52e827385d3cdae6c42e7e670db5c1224a5518fe
Dian Ramdhani Lubis <dian.lubis@prosa.ai>
2022-05-04 02:04:00 +0000

    implement DRLDevfolioCustomizeCounter

--- template-parts/counter.php
+++ template-parts/counter.php
@@ -9,7 +9,7 @@
                         <span class="ico-circle"><i class="bi bi-check"></i></span>
                     </div>
                     <div class="counter-num">
-                        <p data-purecounter-start="0" data-purecounter-end="450" data-purecounter-duration="1" class="counter purecounter"></p>
+                        <p data-purecounter-start="0" data-purecounter-end="<?= get_theme_mod('counter_works', '450') ?>" data-purecounter-duration="1" class="counter purecounter"></p>
                         <span class="counter-text">WORKS COMPLETED</span>
                     </div>
                 </div>
@@ -20,7 +20,7 @@
                         <span class="ico-circle"><i class="bi bi-journal-richtext"></i></span>
                     </div>
                     <div class="counter-num">
-                        <p data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="1" class="counter purecounter"></p>
+                        <p data-purecounter-start="0" data-purecounter-end="<?= get_theme_mod('counter_years', '25') ?>" data-purecounter-duration="1" class="counter purecounter"></p>
                         <span class="counter-text">YEARS OF EXPERIENCE</span>
                     </div>
                 </div>
@@ -31,7 +31,7 @@
                         <span class="ico-circle"><i class="bi bi-people"></i></span>
                     </div>
                     <div class="counter-num">
-                        <p data-purecounter-start="0" data-purecounter-end="550" data-purecounter-duration="1" class="counter purecounter"></p>
+                        <p data-purecounter-start="0" data-purecounter-end="<?= get_theme_mod('counter_clients', '550') ?>" data-purecounter-duration="1" class="counter purecounter"></p>
                         <span class="counter-text">TOTAL CLIENTS</span>
                     </div>
                 </div>
@@ -42,7 +42,7 @@
                         <span class="ico-circle"><i class="bi bi-award"></i></span>
                     </div>
                     <div class="counter-num">
-                        <p data-purecounter-start="0" data-purecounter-end="48" data-purecounter-duration="1" class="counter purecounter"></p>
+                        <p data-purecounter-start="0" data-purecounter-end="<?= get_theme_mod('counter_awards', '48') ?>" data-purecounter-duration="1" class="counter purecounter"></p>
                         <span class="counter-text">AWARD WON</span>
                     </div>
                 </div>
